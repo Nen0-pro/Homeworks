3290f0be786e6cd15e5bab225eca396171599471
Nen0-pro <nenadpopovski@yahoo.com>
2020-03-15 16:20:41 +0100

    initial commit

---
+++ Homework_1/Homework_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Homework1
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("STOP");
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("READY");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("GO");
+            Console.ResetColor();
+            Console.ReadLine();
+        }
+    }
+}
