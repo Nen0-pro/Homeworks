00b7258ad57f70cb200c9f84b7ed58b054f2974b
Nen0-pro <nenadpopovski@yahoo.com>
2020-03-15 16:10:35 +0100

    Initial commit

---
+++ Homework_2/Homework2/Homework2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Homework 2
+
+            // Task 1
+
+            //string firstName = "Nenad";
+            //string lastName = "Popovski";
+
+            //string fullName = firstName + " " + lastName;
+
+            //Console.WriteLine(fullName);
+            //Console.ReadLine();
+
+            //string numberOne = "9";
+            //string numberTwo = "3";
+
+            //string result = numberOne + " " + numberTwo;
+
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+            //// Task 2
+
+            //string name = "Nenad";
+
+            //int num = 10;
+
+            //var res = name + num;
+
+            //Console.WriteLine(res);
+            //Console.ReadLine();
+
+            //// Task 3
+
+            //int n = 102;
+
+            //int m = 5;
+
+            //int sms = n / m;
+
+            //Console.WriteLine(sms);
+            //Console.ReadLine();
+
+            // Task 4
+
+            Console.WriteLine("Enter number");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if (input % 2 == 0)
+            {
+                Console.WriteLine("Even number");
+            }
+            else if(input % 2 !=0)
+            {
+                Console.WriteLine("Odd number");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+    }
+}
